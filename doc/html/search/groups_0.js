f86c03e94eaf9a79a7ef34a1d41f1959c2699b4f
ens18trn <ens18trn@cs.umu.se>
2023-04-04 16:04:08 +0200

    removed some things that shouldnt be there

--- doc/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['my_20module_0',['My module',['../group__module.html',1,'']]]
-];
--- html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['my_20list_0',['my list',['../group__list.html',1,'']]],
-  ['my_20module_1',['My module',['../group__module.html',1,'']]]
-];
